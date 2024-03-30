52e2587099a064226ad043d18fb7615ad6b89fc6
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-03-30 23:25:14 +0100

    Add handleClick events

--- 01tut/src/Content.jsx
+++ 01tut/src/Content.jsx
@@ -8,15 +8,23 @@ const Content = () => {
   };
 
   const handleClick = () => {
-    console.log("====================================");
     console.log("You clicked");
-    console.log("====================================");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
   };
 
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
       <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2("Ahmad")}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   );
 };
