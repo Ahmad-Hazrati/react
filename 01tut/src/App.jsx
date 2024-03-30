2405c75b680dc93d562871d1efb7e28922d17e7f
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-03-30 22:14:30 +0100

    Add handleNameChange function

--- 01tut/src/App.jsx
+++ 01tut/src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
+  const handleNameChange = () => {
+    const names = ["John", "Jane", "Jim", "Ahmad"];
+    const int = Math.floor(Math.random() * names.length);
+    return names[int];
+  };
   return (
     <>
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello from {handleNameChange()}</h1>
     </>
   );
 }
